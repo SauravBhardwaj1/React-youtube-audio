1c95ef7a1685c437ef25eee54f2893356d64f2d7
Saurav Bhardwaj <sauravbhardwaj836@gmail.com>
2024-05-29 16:19:56 +0530

    adding backend with audio extracting functionality

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const ytdl = require('ytdl-core');
 
 const path = require('path');
 const app = express();
-const PORT = process.env.PORT || 8010;
+const PORT = process.env.PORT || 8000;
 
 app.use(cors());
 app.use(express.json());
